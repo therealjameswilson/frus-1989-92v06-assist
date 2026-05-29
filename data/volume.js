64ea281bcfcfc0fcc4f9717f74b2178a146516b0
therealjameswilson <30484292+therealjameswilson@users.noreply.github.com>
2026-05-29 07:02:31 -0400

    Create FRUS assist page

---
+++ data/volume.js
@@ -0,0 +1,15 @@
+window.FRUS_VOLUME = {
+  "index": 13,
+  "doc_id": "frus1989-92v06",
+  "title": "1989\u20131992, Volume VI, Eastern Mediterranean",
+  "official_url": "https://history.state.gov/historicaldocuments/frus1989-92v06",
+  "repo_slug": "frus-1989-92v06-assist",
+  "title_ascii": "1989-1992, Volume VI, Eastern Mediterranean",
+  "pages_url": "https://therealjameswilson.github.io/frus-1989-92v06-assist/",
+  "repo_url": "https://github.com/therealjameswilson/frus-1989-92v06-assist",
+  "nara_scout_url": "https://therealjameswilson.github.io/nara-scout/#q=Eastern+OR+Mediterranean&from=1989&to=1992&sort=relevance&perColl=25&perPage=50",
+  "companion": [
+    "Eastern Mediterranean research assistant",
+    "https://therealjameswilson.github.io/GCT-89-92/"
+  ]
+};
